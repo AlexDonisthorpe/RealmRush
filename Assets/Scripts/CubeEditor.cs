230cea012314520f1258a4e24f8e3a38abe0f5e5
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-06-23 15:24:31 +0100

    Added a label which updates according to the current grid position of the parent cube

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField][Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    private void Awake()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+    }
 
     void Update()
     {
@@ -14,5 +22,6 @@ public class EditorSnap : MonoBehaviour
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+        textMesh.text = snapPos.x/gridSize + "," + snapPos.z/gridSize;
     }
 }
