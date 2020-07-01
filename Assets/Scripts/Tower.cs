51834d005dce4419722d407bd85bbccb7000fb0f
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-07-01 03:03:53 +0100

    Implemented tower enemy tracking & attacking based on range

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -7,10 +7,47 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy;
+    [SerializeField] float attackRange = 100f;
+    [SerializeField] ParticleSystem projectileParticle;
+
+    bool enemyExists = false;
     // Update is called once per frame
     void Update()
     {
-        LookAtEnemy();
+        if (targetEnemy)
+        {
+            LookAtEnemy();
+            FireAtEnemy();
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void ShootLasers()
+    {
+        ParticleSystem lasers = GetComponentInChildren<ParticleSystem>();
+        lasers.Play();
+    }
+
+    private void FireAtEnemy()
+    {
+        float distanceToEnemy = Vector3.Distance(targetEnemy.transform.position, transform.position);
+        if (distanceToEnemy <= attackRange)
+        {
+            Shoot(true);
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void Shoot(bool active)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = active;
     }
 
     private void LookAtEnemy()
