5615df1a54a61896d565d0a48bf62ac1f77a6a88
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-07-01 13:58:09 +0100

    Configured enemy spawns

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns = 6f;
+    [SerializeField] EnemyMovement enemyPrefab;
+
+    void Start()
+    {
+        StartCoroutine("SpawnEnemies");
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true) // todo set end condition
+        {
+            Instantiate(enemyPrefab);
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+}
